bc1109d324807b7411a57fc1498ca50fc35c6eb7
JDMeneses27 <189859220+JDMeneses27@users.noreply.github.com>
2024-12-03 08:11:37 -0500

    python variables y tipos

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print ("Hello World")
+# VARIABLES
+# TIPOS DE DATOS PRIMITIVOS
+# - String = cadenas de caracteres 'str'
+# - Char = Caracter 'str'
+# - Number = Enteros 'int', Decimal 'float'
+# - Bolean = Datos logicos 'bool'
+name = "Juan Meneses"
+age = 16
+height = 1.75
+student = True
+
+# CONJUNTO DE DATOS
+# - Arroy = Conjuntos de datos de una dimension 'list'
+hobbies = list()
+hobbies = ["Jugar","Dormir","Ir al gym","Jugar futbol"]
+
+# MODIFICAR UN DATO DE UNA LISTA
+# hobbies[2] = "Jugar basket"
+
+#SOLICITAR DATOS EN LA CONSOLA Y GUARDARLOS EN LA LISTA
+#dato = input("Ingrese otro pasatiempos: ")
+#hobbies.append(dato)
+#print(hobbies)
+
+# ELIMINAR EL ULTIMO DATO DE LA LISTA
+hobbies.pop()
+print(hobbies)
